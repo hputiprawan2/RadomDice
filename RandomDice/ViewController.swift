76a3918107e91a4364bfefb2fabbe8d7d309ac26
Hanna Putiprawan <hanna.p@apple.com>
2021-01-10 15:29:52 -0800

    Initial Commit

---
+++ RandomDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomDice
+//
+//  Created by Hanna Putiprawan on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
