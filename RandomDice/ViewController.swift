75f69860fb9de48f1ce0cf3206b604242510d197
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-01-10 17:26:35 -0800

    [Add] Random Dice App

--- RandomDice/ViewController.swift
+++ RandomDice/ViewController.swift
@@ -1,19 +1,25 @@
 //
 //  ViewController.swift
-//  RandomDice
+//  Dicee-iOS13
 //
-//  Created by Hanna Putiprawan on 1/10/21.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArray.randomElement() // Random element in the array
+        diceImageView2.image = diceArray[Int.random(in: 0...5)] // Random from index 0 to 5
+        // The result should be the same for both random
     }
-
-
+    
 }
 
